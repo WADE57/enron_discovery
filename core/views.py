27bf050d655135f7c6beb91840a4eb1901c9f436
WADE <abdouwade1996@gmail.com>
2026-03-12 22:51:04 +0100

    mon premier de test de Django

--- core/views.py
+++ core/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+#exple hello world
+def hello_world(request):
+    return render(request, 'hello_world.html', {'message': 'Hello, World!'})
--- enron_discovery/urls.py
+++ enron_discovery/urls.py
@@ -15,8 +15,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from core.views import hello_world  # Import the hello_world view
 
+# urlpatterns = [
+#     path('admin/', admin.site.urls),
+# ]
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    # path("polls/", include("polls.urls")),
+    path("admin/", admin.site.urls),
+    path("", hello_world, name="home"),  # Include URLs from the core app
 ]
