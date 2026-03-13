9dd8c3d5783a012db7c6b2fc53aef61e4f6d8548
WADE <abdouwade1996@gmail.com>
2026-03-13 12:01:56 +0100

    creation de 0001_initial.py

--- enron_discovery/urls.py
+++ enron_discovery/urls.py
@@ -17,7 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-from polls.views import hello_world  # Import the hello_world view
+# from polls.views import hello_world  # Import the hello_world view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
