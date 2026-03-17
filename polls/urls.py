8d12e8ff6f11925d8377aced04d13ba906112b01
WADE <abdouwade1996@gmail.com>
2026-03-18 00:22:48 +0100

    modification de polls/urls.py

--- polls/urls.py
+++ polls/urls.py
@@ -1,10 +1,3 @@
-# from django.urls import path
-# from . import views
-
-# urlpatterns = [
-#     path('', views.email_list, name='email_list'),
-# ]
-
 from django.urls import path
 from . import views
 
@@ -13,4 +6,5 @@ urlpatterns = [
     path("dashboard/", views.dashboard, name="dashboard"),
     path("search/", views.search_emails, name="search_emails"),
     path("threads/<int:email_id>/", views.thread_detail, name="thread_detail"),
+    path("influence/", views.influence_graph, name="influence_graph"),
 ]
